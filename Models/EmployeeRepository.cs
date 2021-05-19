e405c9f7d0262bd298295317a892592dd4a52c62
Sumit Kumar Singh <sumitit.ikrab@gmail.com>
2021-05-19 17:48:30 +0530

    initial

---
+++ Models/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_WebApplication.Models
+{
+    public class EmployeeRepository
+    {
+        public List<Department> GetDepartments()
+        {
+            EmployeeModel employeeModelDBContext = new EmployeeModel();
+            return employeeModelDBContext.Departments.ToList();
+        }
+    }
+}
